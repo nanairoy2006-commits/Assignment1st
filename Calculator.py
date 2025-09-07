cc8c0893744430d86c38613e12cf63bb563be445
nanairoy2006-commits <nanairoy2006@gmail.com>
2025-09-07 10:10:23 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+a=float(input('Enter the first no.'))
+b=float(input('Enter the second no.'))
+
+A=(a+b)
+S=(a-b)
+M=(a*b)
+D=(a/b)
+print('Addition:',A)
+
+print('Substraction',S)
+
+print('Multiplication',M)
+
+print('Division',D)
---
+++ Name.py
@@ -0,0 +1,5 @@
+a=input('Enter the First name:')
+b=input('Enter the Second name:')
+
+
+print(a+'\t'+b,'Welcome to the python program')
